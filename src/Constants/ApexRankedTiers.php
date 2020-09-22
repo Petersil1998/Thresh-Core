528b37e8b47a43703081f81c138b03ca501bae90
Petersil1998 <silbernagl.peter@rolmail.net>
2020-09-22 15:42:53 +0200

    Added Apex Ranked tiers

---
+++ src/Constants/ApexRankedTiers.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Thresh_Core\Constants;
+
+use ReflectionClass;
+
+class ApexRankedTiers
+{
+    const MASTER = 'MASTER';
+    const GRANDMASTER = 'GRANDMASTER';
+    const CHALLENGER = 'CHALLENGER';
+
+    /**
+     * @return array
+     */
+    public static function getApexTiers(){
+        $reflection = new ReflectionClass(__CLASS__);
+        return $reflection->getConstants();
+    }
+}
