bda70e81588d4217ed54496626853cc5df7c375c
Petersil1998 <silbernagl.peter@rolmail.net>
2020-09-30 16:29:45 +0200

    Added Clean Platforms

---
+++ src/Constants/PlatformsClean.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Thresh_Core\Constants;
+
+use ReflectionClass;
+
+class PlatformsClean
+{
+    const EUW = 'euw';
+    const EUNE = 'eune';
+    const KR = 'kr';
+    const JP = 'jp';
+    const OCE = 'oce';
+    const BR = 'br';
+    const RU = 'ru';
+    const TR = 'tr';
+    const NA = 'na';
+    const LAN = 'lan';
+    const LAS = 'las';
+
+    /**
+     * @return array
+     */
+    public static function getPlatforms(){
+        $reflection = new ReflectionClass(__CLASS__);
+        return $reflection->getConstants();
+    }
+}
